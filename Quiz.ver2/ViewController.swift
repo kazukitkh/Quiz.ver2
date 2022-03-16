f9d0ecdd14f54540aee6711de4d6042caa929a08
kazukitkh <kazuki.tkh@gmail.com>
2022-03-17 04:33:35 +0900

    Initial Commit

---
+++ Quiz.ver2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Quiz.ver2
+//
+//  Created by 武樋一樹 on 2022/03/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Quiz.ver2UITests/Quiz_ver2UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Quiz_ver2UITestsLaunchTests.swift
+//  Quiz.ver2UITests
+//
+//  Created by 武樋一樹 on 2022/03/17.
+//
+
+import XCTest
+
+class Quiz_ver2UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
